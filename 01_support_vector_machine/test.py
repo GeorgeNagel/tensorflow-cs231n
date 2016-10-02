bb3d3d0ff47ffcec36990fc3492a69606a37a0a7
George Nagel <gsnagel@gmail.com>
2016-10-02 16:18:43 -0400

    Testing vectorized loss function

--- 01_support_vector_machine/test.py
+++ 01_support_vector_machine/test.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 import numpy as np
 
-from main import numerical_gradient
+from main import numerical_gradient, vectorized_loss
 
 
 class TestGradient(TestCase):
@@ -22,3 +22,14 @@ class TestGradient(TestCase):
 
         expected_grad_b = np.array([-1, 1], dtype=np.float64)
         self.assertTrue(np.allclose(grad_b, expected_grad_b))
+
+
+class TestVectorizedLoss(TestCase):
+    def test_loss(self):
+        W = np.array([[1, 2, 3], [4, 5, 6]], dtype=np.float64)
+        b = np.array([1, 2], dtype=np.float64)
+        x = np.array([1, 2, 3], dtype=np.float64)
+        correct_class_index = 0
+
+        loss = vectorized_loss(x, correct_class_index, W, b)
+        self.assertTrue(np.isclose(loss, 20.0))
