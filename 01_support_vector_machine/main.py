bfa586c9f209fbdfd79b631a8de1c42fcf0c9735
George Nagel <gsnagel@gmail.com>
2016-10-02 13:13:23 -0400

    Work in progress. Numpy implementation of SVM on iris dataset.

---
+++ 01_support_vector_machine/main.py
@@ -0,0 +1,99 @@
+# This file trains a Support Vector Machine (SVM) on the iris dataset
+from tensorflow.contrib import learn
+from sklearn import cross_validation
+import numpy as np
+
+# The output y will be calculated via
+# y = W * x + b
+iris = learn.datasets.load_dataset('iris')
+x_train, x_test, y_train, y_test = cross_validation.train_test_split(
+    iris.data, iris.target, test_size=0.2, random_state=123)
+
+# The loss function for an SVM
+# Li = Sum<j!=yi>(max(0, sj - sy + 1)) + L1norm(W)
+# We can derive the gradient from this
+
+# We're going to do the train/backprop loop in numpy
+
+def vectorized_loss(x, correct_class_index, W):
+    scores = np.dot(W, x)
+    margins = np.maximum(0, scores - scores[correct_class_index] + 1)
+    # Setting the correct class margin to zero after the fact
+    # makes it easier to take advantage of the vectorized approach as above
+    margins[correct_class_index] = 0
+    loss = np.sum(margins)
+    return loss
+
+def analytic_gradient(W, x, b, y_expected):
+    """
+    Return the gradient for W and b given an input x and expected y
+    """
+    # Forward pass
+    W_dot_x = np.dot(W, x)
+    y_actual = np.add(W_dot_x, b) 
+    
+    l_1_norm = np.linalg.norm(W, ord=1)
+    correct_class_index = np.argmax(y_expected)
+    # Get the score for the correct class
+    sj = y_expected[correct_class_index]
+    # Create an array with the value of the correct score
+    sj_array = np.mult(np.ones(3), sj)
+
+    s_diff = np.subtract(sj_array, y_actual)
+    s_diff_biased = np.add(s_diff, 1)
+    s_diff_biased_clamped = np.maximum(s_diff_biased, 0)
+    score_loss = np.sum(s_diff_biased_clamped)
+    loss = score_loss + l_1_norm
+
+    # Back propogation
+    d_loss_d_loss = 1
+    # Calculate the gradient contribution of the regularization step
+    d_W_d_l_1_norm
+
+    return W_grad, b_grad
+
+def numerical_gradient(func, x):
+    """
+    Estimate the gradient of function func at point x (a numpy vector).
+    """
+    # Evaluate the funcation at the original point.
+    fx = f(x)
+    gradient = np.zeros(x.shape)
+    h = 0.0001
+
+    # Iterate over all indexes in x
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwriter'])
+    while not it.finished:
+        # Evaluate funcation at x + h
+        ix = it.multi_index
+        old_value = x[ix]
+        # Increment by h
+        x[ix] = old_value + h
+        # Evaluate f(x + h)
+        fxh = f(x)
+        # Restore to previous value
+        x[ix] = old_value
+
+        # Compute the partial derivative
+        gradient[ix] = (fxh - fx) / h
+        # Step to next dimension
+        it.iternext()
+    return gradient
+
+def predict(W, x, b):
+    linear_result = np.dot(W, x)
+    linear_with_bias = linear_result + b
+    best_class = np.argmax(linear_with_bias)
+    y_predicted = np.zeros(3)
+    return y_predicted
+
+import pdb
+pdb.set_trace()
+
+# Each x sample has four dimensions, and belongs to one of three possible classes
+# By dimensional analysis, since W must be multiplied by x and added to an array to get y, W must be 4x3.
+# 3x4 * 4x1 + 3x1 => 3x1
+W_initial = np.zeros([3, 4])
+# Similarly, b must be a 3x1 array
+b_initial = np.zeros([3, 1])
+
