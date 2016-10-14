4cc88f83291d3d0a04e3fdf21c30e43746fae597
George Nagel <gsnagel@gmail.com>
2016-10-14 14:27:05 -0400

    Double-checking gradients using numerical approach.

--- 02_svm_net/main.py
+++ 02_svm_net/main.py
@@ -1,6 +1,35 @@
 import numpy as np
 
 
+def numerical_gradient(func, x):
+    """
+    Estimate the gradient of function func at point x (a numpy vector).
+    """
+    # Evaluate the funcation at the original point.
+    initial = func(x)
+    grad_x = np.zeros(x.shape)
+    h = 0.0001
+
+    # Iterate over all indexes in x
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        # Evaluate loss at x + h
+        ix = it.multi_index
+        old_value = x[ix]
+        # Increment by h
+        x[ix] = old_value + h
+        # Evaluate loss(x + h)
+        func_xh = func(x)
+        # Restore to previous value
+        x[ix] = old_value
+
+        # Compute the partial derivative
+        grad_x[ix] = (func_xh - initial) / h
+        # Step to next dimension
+        it.iternext()
+    return grad_x
+
+
 class AdditionNode(object):
     def forward(self, input_1, input_2):
         self.input_1 = input_1
--- 02_svm_net/test.py
+++ 02_svm_net/test.py
@@ -2,7 +2,27 @@ from unittest import TestCase
 
 import numpy as np
 
-from main import AdditionNode
+from main import AdditionNode, numerical_gradient
+
+
+class TestNumericalGradient(TestCase):
+    def test_sumall_gradient(self):
+        input_vector = np.ones([3, 5])
+        fn_to_optimize = lambda x: np.sum(x)
+
+        actual_gradient = numerical_gradient(fn_to_optimize, input_vector)
+
+        expected_gradient = np.ones([3, 5])
+        np.testing.assert_allclose(expected_gradient, actual_gradient)
+
+    def test_sumall_gradient_negative_input(self):
+        input_vector = -1 * np.ones([3, 5])
+        fn_to_optimize = lambda x: np.sum(x)
+
+        actual_gradient = numerical_gradient(fn_to_optimize, input_vector)
+
+        expected_gradient = np.ones([3, 5])
+        np.testing.assert_allclose(expected_gradient, actual_gradient)
 
 
 class TestAdditionNode(TestCase):
@@ -10,11 +30,11 @@ class TestAdditionNode(TestCase):
         self.input_1 = np.array([
             [1, 2, 3],
             [4, 5, 6]
-        ])
+        ], dtype=np.float64)
         self.input_2 = np.array([
             [4, 5, 6],
             [7, 8, 9]
-        ])
+        ],  dtype=np.float64)
         self.node = AdditionNode()
 
     def test_forward(self):
@@ -25,10 +45,10 @@ class TestAdditionNode(TestCase):
             np.array([
                 [5, 7, 9],
                 [11, 13, 15]
-            ])
+            ], dtype=np.float64)
         )
 
-    def test_local_gradients(self):
+    def test_local_gradients_analytic(self):
         self.node.forward(self.input_1, self.input_2)
 
         gradients = self.node.gradients()
@@ -41,5 +61,25 @@ class TestAdditionNode(TestCase):
         )
         np.testing.assert_allclose(
             gradients[1],
+            np.ones([2, 3])
+        )
+
+    def test_local_gradients_numerical(self):
+        def loss_1(input_1):
+            out = self.node.forward(input_1, self.input_2)
+            return np.sum(out)
+
+        def loss_2(input_2):
+            out = self.node.forward(self.input_1, input_2)
+            return np.sum(out)
+        gradient_1 = numerical_gradient(loss_1, self.input_1)
+        gradient_2 = numerical_gradient(loss_2, self.input_2)
+
+        np.testing.assert_allclose(
+            gradient_1,
+            np.ones([2, 3])
+        )
+        np.testing.assert_allclose(
+            gradient_2,
             np.ones([2, 3])
         )
