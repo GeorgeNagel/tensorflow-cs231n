46e959f74b0ddc37356507a9190eb0affca0e0e0
George Nagel <gsnagel@gmail.com>
2016-10-14 14:49:45 -0400

    Testing numerical gradient on multiplication function.

--- 02_svm_net/main.py
+++ 02_svm_net/main.py
@@ -1,14 +1,13 @@
 import numpy as np
 
 
-def numerical_gradient(func, x):
+def numerical_gradient(func, x, step_size=0.0001):
     """
     Estimate the gradient of function func at point x (a numpy vector).
     """
     # Evaluate the funcation at the original point.
     initial = func(x)
     grad_x = np.zeros(x.shape)
-    h = 0.0001
 
     # Iterate over all indexes in x
     it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
@@ -17,14 +16,14 @@ def numerical_gradient(func, x):
         ix = it.multi_index
         old_value = x[ix]
         # Increment by h
-        x[ix] = old_value + h
+        x[ix] = old_value + step_size
         # Evaluate loss(x + h)
         func_xh = func(x)
         # Restore to previous value
         x[ix] = old_value
 
         # Compute the partial derivative
-        grad_x[ix] = (func_xh - initial) / h
+        grad_x[ix] = (func_xh - initial) / step_size
         # Step to next dimension
         it.iternext()
     return grad_x
@@ -40,3 +39,13 @@ class AdditionNode(object):
         gradient_1 = np.ones(self.input_1.shape)
         gradient_2 = np.ones(self.input_2.shape)
         return [gradient_1, gradient_2]
+
+
+class MultiplicationNode(object):
+    def forward(self, input_1, input_2):
+        self.input_1 = input_1
+        self.input_2 = input_2
+        return np.dot(self.input_1, self.input_2)
+
+    def gradients(self):
+        return [self.input_2, self.input_1]
--- 02_svm_net/test.py
+++ 02_svm_net/test.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 
 import numpy as np
 
-from main import AdditionNode, numerical_gradient
+from main import AdditionNode, MultiplicationNode, numerical_gradient
 
 
 class TestNumericalGradient(TestCase):
@@ -82,4 +82,49 @@ class TestAdditionNode(TestCase):
         np.testing.assert_allclose(
             gradient_2,
             np.ones([2, 3])
+        )
+
+
+class TestMultiplicationNode(TestCase):
+    def setUp(self):
+        self.input_1 = np.array([
+            [1, 2, 3],
+            [4, 5, 6]
+        ], dtype=np.float64)
+        self.input_2 = np.array([
+            [7, 8, 9],
+            [10, 11, 12],
+            [13, 14, 15]
+        ], dtype=np.float64)
+        self.node = MultiplicationNode()
+
+    def test_local_gradients_numerical(self):
+        step_size = .1 ** 7
+
+        def loss_1(input_1):
+            out = self.node.forward(input_1, self.input_2)
+            return np.sum(out)
+
+        def loss_2(input_2):
+            out = self.node.forward(self.input_2, input_2)
+            return np.sum(out)
+        gradient_1 = numerical_gradient(
+            loss_1, self.input_1, step_size=step_size)
+        gradient_2 = numerical_gradient(
+            loss_2, self.input_2, step_size=step_size)
+
+        np.testing.assert_allclose(
+            gradient_1,
+            np.array([
+                [24, 33, 42],
+                [24, 33, 42]
+            ], dtype=np.float64)
+        )
+        np.testing.assert_allclose(
+            gradient_2,
+            np.array([
+                [54, 63, 72],
+                [57, 66, 75],
+                [60, 69, 78]
+            ], dtype=np.float64)
         )
